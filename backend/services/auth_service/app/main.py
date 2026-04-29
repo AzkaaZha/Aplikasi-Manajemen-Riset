3ae3616960b28800e54733b89b40b69e77c3e100
AzkaaZha <azka22060ti@student.nurulfikri.ac.id>
2026-04-29 12:12:16 +0700

    Revisi Auth Service

--- backend/services/auth_service/app/main.py
+++ backend/services/auth_service/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
 
 from app.core.config import APP_NAME
@@ -8,6 +9,19 @@ from app.api.routes.auth import router as auth_router
 from app.models import Role, User
 
 app = FastAPI(title=APP_NAME)
+
+origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(health_router)
 app.include_router(user_router)
